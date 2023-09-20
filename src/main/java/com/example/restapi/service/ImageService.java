148417f33f1b10670dc6eb67472b115b45bc30d0
Ignat Ignatov <ignatov_rs@abv.bg>
2023-09-20 11:09:13 +0300

    remove unusable imports

--- src/main/java/com/example/restapi/service/ImageService.java
+++ src/main/java/com/example/restapi/service/ImageService.java
@@ -1,14 +1,11 @@
 package com.example.restapi.service;
 
-import com.example.restapi.model.ImageEntity;
-import com.example.restapi.model.UserEntity;
 import com.example.restapi.model.dto.imageDto.ImageResponse;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
 public interface ImageService {
 
